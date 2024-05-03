1ca3347705b6e9778107061ea74c1c04f07866cb
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2024-05-03 16:30:32 +0200

    added a modal for conference info

--- public/main.js
+++ public/main.js
@@ -31,3 +31,23 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const openModalBtn = document.getElementById("openModalBtn");
+  const modal = document.getElementById("modal");
+  const closeModalBtn = modal.querySelector(".close");
+
+  openModalBtn.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+
+  closeModalBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+});
